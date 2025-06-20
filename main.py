8172864de7ad5b0cdd28ae2be1ced14d7c403eb3
sunnychowdhury <sca432@gmail.com>
2025-06-20 15:21:31 -0400

    Added swapping numbers without using a third variable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-#1. Swap two numbers
+# EASY LEVEL 
+# Variables and Data Types
+
+#1. Swap two numbers - Using Tuple
 
 num1 = input('Enter the first number: ')
 num2 = input('Enter the second number: ')
@@ -24,6 +27,22 @@ print('Second number: ', num2)
 num3 = num1
 num1 = num2
 num2 = num3
+
+print('After swapping')
+print('First number: ', num1)
+print('Second number: ', num2)
+
+#1b. Swap two numbers - Without using a third variable
+
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+print('First number: ', num1)
+print('Second number: ', num2)
+
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
 
 print('After swapping')
 print('First number: ', num1)
