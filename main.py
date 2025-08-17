04f262719006c9de23c6374d328a9f5524dafa49
sunnychowdhury <sca432@gmail.com>
2025-08-16 22:09:14 -0700

    Merge Strings Alternately

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ Leetcode: Two Pointers : Easy
 -----------------------------
 125. Valid Palindrome  ✅
 680. Valid Palindrome II ✅
+1768. Merge Strings Alternately
 
 Leetcode: Sliding Window : Easy
 -------------------------------
@@ -983,6 +984,25 @@ def validPalindromeTwo(s):
     return True
 
 print(validPalindromeTwo(s))
+
+#1768. Merge Strings Alternately
+word1 = "abc"
+word2 = "pqr"
+
+def mergeStringsAlternately(word1, word2):
+    res = []
+    i, j = 0, 0
+    while i < len(word1) and j < len(word2):
+        res.append(word1[i])
+        res.append(word2[j:])
+        i += 1
+        j += 1
+    res.append(word1[i:])
+    res.append(word2[j:])
+
+    return "".join(res)
+
+print(mergeStringsAlternately(word1, word2))
 
 # Leetcode: Sliding Window : Easy
 # -------------------------------
